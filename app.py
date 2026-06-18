512fa523fc161efbb97bad6372d39187c08734cf
Nagabharath1234 <bharathchowdhary79@gmail.com>
2026-06-18 23:46:16 +0530

    Add user registration functionality in app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+print("===== User Registration =====")
+
+name = input("bharath ")
+email = input("bharath@gmail.com ")
+phone = input("9999999999 ")
+password = input("bharath678 ")
+
+print("\n===== Registration Successful =====")
+print("Name:", name)
+print("Email:", email)
+print("Phone:", phone)
