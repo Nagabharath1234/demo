efc34f175adaf9af439f2894253c8bfe2f8eb7ba
Nagabharath1234 <bharathchowdhary79@gmail.com>
2026-06-18 23:58:03 +0530

    Add greeting message to app.py

--- app.py
+++ app.py
@@ -1,11 +1,2 @@
-print("===== User Registration =====")
-
-name = input("Enter Name: ")
-email = input("Enter Email: ")
-phone = input("Enter Phone Number: ")
-password = input("Enter Password: ")
-
-print("\n===== Registration Successful =====")
-print("Name:", name)
-print("Email:", email)
-print("Phone:", phone)
+a="hello world"
+print(a)
